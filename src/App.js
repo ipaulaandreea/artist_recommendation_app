8c68cca5d721f7f3e21b664ee6841d4f433a54b1
ipaulaandreea <andreeapaulaivan@gmail.com>
2023-08-29 07:55:22 +0300

    wip api config

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import classes from './App.module.css'
-import React, { useState } from 'react'
-import { Container, Typography, Grid } from '@mui/material'
+import React, { useState, useEffect } from 'react'
+// import { Container, Typography, Grid } from '@mui/material'
 import { styled } from '@mui/system'
 import SearchResults from './components/SearchResults/SearchResults'
 import Header from './components/Header/Header'
 import RecommendationCard from './components/RecommendationCard/RecommendationCard'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Greeting from './components/Greeting/Greeting'
+import axios from 'axios';
 
 const Background = styled('div')({
   position: 'absolute',
@@ -19,13 +20,12 @@ const Background = styled('div')({
 })
 
 function App() {
-  const CLIENT_ID = '6674cfd3c7f24b579bdf58acd6f13d95'
-  const REDIRECT_URI = 'http://localhost:3000/'
-  const AUTH_ENDPOINT = 'https://accounts.spotify.com/authorize'
-  const RESPONSE_TYPE = 'token'
+
 
   const [displayResults, setDisplayResults] = useState(false)
 
+
+  
   const displayResultsHandler = () => {
     setDisplayResults(true)
   }
@@ -34,15 +34,14 @@ function App() {
     setDisplayResults(false)
   }
 
+
   return (
     <Background>
-      <a
-        href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}
-      >
-        Login to Spotify>
-      </a>
+     
+
       {displayResults && <SearchResults onHideResults={hideResultsHandler} />}
       <Greeting onDisplayResults={displayResultsHandler} />
+      
     </Background>
   )
 }
--- src/components/Greeting/Greeting.js
+++ src/components/Greeting/Greeting.js
@@ -1,8 +1,50 @@
-import react from 'react';
+import React, { useState, useEffect } from 'react'
 import { Container, Row, Col, Image, Card, Form, Button  } from 'react-bootstrap';
 import backgroundImage from '../../bckg3.jpg'
 import {classes} from './Greeting.module.css'; 
+import axios from 'axios';
+
+
 const Greeting = (props) => {
+  const CLIENT_ID = '6674cfd3c7f24b579bdf58acd6f13d95'
+  const REDIRECT_URI = 'http://localhost:3000/'
+  const AUTH_ENDPOINT = 'https://accounts.spotify.com/authorize'
+  const RESPONSE_TYPE = 'token'
+
+  const [token, setToken]=useState('')
+  const [searchKey, setSearchKey]=useState('')
+
+  useEffect(()=>{
+    const hash=window.location.hash
+    let token=window.localStorage.getItem('token')
+    if (!token&&hash){
+      token=hash.substring(1).split("&").find(elem=>elem.startsWith("access_token")).split("=")[1]
+      window.location.hash="";
+      window.localStorage.setItem("token", token);
+      
+    }
+    setToken(token)
+  }, [])
+
+  const logout = () => {
+    setToken('');
+    window.localStorage.removeItem('token');
+  }
+
+  const searchArtists = async (e) => {
+    const {data} = await axios.get("https://api.spotify.com/v1/search"), {
+      headers: {
+        Authorization: `Bearer ${token}`
+      },
+      params: {
+         q: searchKey,
+         type: 'artist'        
+        }
+    }
+    console.log(data);
+  }
+
+
     return ( 
        <section className="bg-light text-center h-100 d-flex align-items-center justify-content-center" style={{backgroundImage: `url(${backgroundImage})`,  height: '800px',  backgroundPosition: 'center', backgroundSize: 'cover', }}>
    
@@ -11,15 +53,23 @@ const Greeting = (props) => {
         <div className="col-md-6 ">
             <h1 className="display-5 fw-bold p-5" style={{color: 'White' }} >Find similar artists. See their gigs.</h1>
             </div>
+
+            {!token ?       
+      <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>
+        Login to Spotify</a>
+      : <button onClick={logout}>Logout</button>
+        }
+
             <div className="col-md-6">
-            {/* <Form>
-        <Form.Group className="p-5">
+            <Form onSubmit={searchArtists}>
+              <input type="text" onChange={e => setSearchKey(e.target.value)}/>
+        {/* <Form.Group className="p-5">
         <Form.Control className="rounded-pill" type="artist-name" placeholder="Artist name" />
       </Form.Group> */}
       <Button onClick={props.onDisplayResults} type="submit" style={{fontSize: '1rem', fontWeight: '700', fontSize: '1rem', border: '0', borderRadius: '0', background: '#ff3366', padding: '16px 40px', color: 'White' }}>
         SEARCH
       </Button>
-    {/* </Form> */}
+    </Form>
     </div>
     </div>
     </div>
